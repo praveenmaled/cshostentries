7d60249315d6a547c46f10054be26552957f5f53
Praveen <praveen.maled@happiestminds.com>
2015-06-25 15:42:02 +0530

    adding sh file

---
+++ EditDNS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "10.40.11.52 devadmin.clicksafety.com">> /etc/hosts
+echo "10.40.11.52  www.clicksafety.com">> /etc/hosts
+echo "10.40.11.52  secure.clicksafety.com">> /etc/hosts
+echo "10.40.11.52  safetypassport.clicksafety.com">> /etc/hosts
+echo "10.40.11.52 www.outreachtrainers.org">> /etc/hosts
+echo "10.40.11.52 outreachtrainers.org">> /etc/hosts
